ad5d2c0d5fcda5f304900e14006b2ebb53edb8e2
laxminarayana33 <laxminarayanakota8@gmail.com>
2022-07-28 18:24:57 +0530

    assignment3

--- js/Home.js
+++ js/Home.js
@@ -2,146 +2,176 @@ var app = angular.module('myApp', [])
 app.controller('myCtrl', function ($scope) {
   $scope.searchItems = [
     {
-      section: 'men|boys',
+      image: 'images/shirt1.jpg',
+      section: 'boys | mens',
       id: 1,
-      name: 'laxminarayana',
-      desc: 'laxminarayanadfsklfje',
-      Image: 'image url',
-      price: 'product pfjlksdjfsdjfoijfrice',
+      name: 'Blue Jeans Shirt',
+      desc: 'Well nice Blue Jeans for perfect men',
+      price: '350',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 2,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Red Shirt',
+      desc: 'A Colour Full Half Hands for Summer',
+      image: 'images/shirt6.jpg',
+      price: '450',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 3,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Jeans T Shirt',
+      desc: 'A Trendy Jeans T Shirft of Full Hands',
+      image: 'images/shirt3.jpg',
+      price: '600',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 4,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Blue Shirt',
+      desc: 'A Perfect Party Ware for Mens',
+      image: 'images/shirt5.jpg',
+      price: '550',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 5,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Red Shirt',
+      desc: 'An Official Office ware for Mens',
+      image: 'images/shirt7.jpg',
+      price: '500',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 6,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Navy Blue Shirt',
+      desc: 'Causal Ware of some Events ',
+      image: 'images/shirt2.jpg',
+      price: '500',
     },
     {
-      section: 'men|boys',
+      section: 'boys | mens',
       id: 7,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Black Shirt',
+      desc: 'For a best trip in Summer ',
+      image: 'images/shirt4.jpg',
+      price: '600',
     },
     {
-      section: 'womens|girls',
+      section: 'boys | mens',
       id: 8,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Kurta Style',
+      desc: 'Best Kurtas for Family Events',
+      image: 'images/kurtha.jpg',
+      price: '600',
     },
     {
       section: 'womens|girls',
       id: 9,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Gown',
+      desc: 'Beautiful Gown For Party',
+      image: 'images/dress1.jpg',
+      price: '900',
     },
     {
       section: 'womens|girls',
       id: 10,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Pink Top',
+      desc: 'Causal Ware for Tours',
+      image: 'images/dress2.jpg',
+      price: '800',
     },
     {
       section: 'womens|girls',
       id: 11,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Chudidar',
+      desc: 'A nice Light Red Chudidar ',
+      image: 'images/dress3.jpg',
+      price: '750',
     },
     {
       section: 'womens|girls',
       id: 12,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Saree',
+      desc: 'Represents the Indian Traditions',
+      image: 'images/dress4.jpg',
+      price: '1000',
     },
     {
       section: 'womens|girls',
       id: 13,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Gree Colour Chudidar',
+      desc: 'The best Out Fit For Causal Events',
+      image: 'images/dress5.jpg',
+      price: '800',
     },
     {
       section: 'womens|girls',
       id: 14,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Black Dress',
+      desc: 'Offical Party Ware for Ladies',
+      image: 'images/dress6.jpg',
+      price: '1000',
     },
     {
       section: 'womens|girls',
       id: 15,
-      name: 'product name',
-      desc: 'product description',
-      Image: 'image url',
-      price: 'product price',
+      name: 'Skirt',
+      desc: 'A Normal Skirts for Meetings',
+      image: 'images/dress7.jpg',
+      price: '850',
     },
   ]
   $scope.foundList = []
   $scope.message = ''
   $scope.isVisible = false
-  $scope.show = function (ite) {
+  $scope.show = function () {
     $scope.isVisible = $scope.isVisible ? fasle : true
   }
-})
 
-app.filter('searchFor', function () {
-  return function (arr, searchString) {
-    if (!searchString) {
-      return arr
-    }
-    var result = []
-    searchString = searchString.toLowerCase()
-    angular.forEach(arr, (item) => {
-      if (item.section.toLowerCase().indexOf(searchString) !== -1) {
-        result.push(item)
-      }
-    })
-    return result
+  $scope.store = function(item){
+    if(item){
+    $scope.foundList.push({section:item.section, id:item.id, name:item.name, desc:item.desc, image:item.image, price:item.price})
+    // console.log($scope.foundList)
+   }
+ 
   }
-})
+ 
+});
+
+// app.config(function($routerProvider){
+//   $routerProvider
+//   .when('/foundlist',{
+//     templateUrl: "cart.html"
+//   });
+// })
+
+
+
+// app.filter('section', function(){
+//   return function(section){
+//     if(section == 'men'){
+//       return searchItems.section;
+//     }
+//     else{
+//       return searchItems.section
+//     }
+//   }
+// })
+
+
+// app.filter('searchFor', function () {
+//   return function (arr, searchString) {
+//     if (!searchString) {
+//       return arr
+//     }
+//     var result = []
+//     searchString = searchString.toLowerCase()
+//     angular.forEach(arr, (item) => {
+//       if (item.section.toLowerCase().indexOf(searchString) !== -1) {
+//         result.push(item)
+//       }
+//     })
+//     return result
+//   }
+// })
